9a27fc43f50bc7ad6c438b547eba2dbf9b1be584
baehaky <baehaky1504@gmail.com>
2024-03-02 22:32:17 +0700

    modified:   src/layout/LoginPage.jsx

--- src/layout/LoginPage.jsx
+++ src/layout/LoginPage.jsx
@@ -1,51 +1,46 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/9.png";
 export default function LoginPage() {
   return (
     <div className="flex min-h-[100dvh]">
       <div className="flex flex-row w-full">
-        <div className="relative overflow-hidden md:flex  w-1/2 bg-teal-400 i justify-around items-center hidden">
-          <div className=" py-10 px-10 rounded-lg backdrop-blur-lg">
-            <div className="">
-              <Link
-                to="/"
-                className="flex items-center space-x-3 hover:underline text-gray-100 rounded transition duration-150"
-              >
-                <span className="bg-white rounded-full">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    viewBox="0 0 20 20"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
-                </span>
-                <span>Kembali ke Beranda</span>
-              </Link>
-            </div>
-            <div>
-              <h1 className="text-white font-bold text-4xl font-sans">
-                Halo Pet
-              </h1>
+        <div className="relative overflow-hidden md:flex w-1/2 flex-col bg-teal-400 justify-center hidden">
+          <div className="px-10 relative -top-24">
+            <Link
+              to="/"
+              className="flex items-center space-x-3 hover:underline text-gray-100 rounded transition duration-150"
+            >
+              <span className="bg-white rounded-full">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </span>
+              <span>Kembali ke Beranda</span>
+            </Link>
+          </div>
+          <div className="py-10 -mt-10 px-10 rounded-lg backdrop-blur-lg">
+            <div className="flex flex-col items-center">
+              <img src={logo} className="w-1/3" />
               <p className="text-white mt-1">
                 Silahkan register jika anda belum memiliki akun
               </p>
               <Link
                 to="/register"
-                className="block text-center w-28 bg-white hover:bg-slate-400 hover:text-white text-indigo-800 mt-4 py-2 rounded-2xl font-bold mb-2"
+                className="block text-center w-1/2 bg-white hover:bg-slate-400 hover:text-white text-black mt-4 rounded-lg font-medium mb-2 px-3 py-2 md:px-4 md:py-3"
               >
                 Registrasi
               </Link>
             </div>
+            <div></div>
           </div>
-          <div className="absolute -bottom-32 -left-40 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
-          <div className="absolute -bottom-14 -left-40 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
-          <div className="absolute -bottom-40 -left-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
-          <div className="absolute -top-40 -right-0 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
-          <div className="absolute -top-20 -right-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
         </div>
         <div className="flex flex-1 flex-col items-center justify-center px-10 relative">
           <div className="flex flex-1 flex-col  justify-center space-y-5 max-w-md">
